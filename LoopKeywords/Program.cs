f41b79539474d9fa0d97c6505c6db5ce556d59c5
Barış Emre Yalçın <barisemre.bey@gmail.com>
2024-08-12 12:44:48 +0300

    initial commit

---
+++ LoopKeywords/Program.cs
@@ -0,0 +1,27 @@
+﻿
+// See https://aka.ms/new-console-template for more information
+
+#region Loop Keywords (break, continue)
+// TASK: 1'den başlayarak kullanıcının girdiği tam sayıya kadar (girilen dahil) tek sayıları ekrana yazdıran program (step: 1)
+//Console.Write("Enter a number: ");
+//bool isNumber = int.TryParse(Console.ReadLine(), out int inputNumber);
+//int sumOdd = 0;
+//if (isNumber)
+//{
+//    for (int i = 0; i <= inputNumber; i++)
+//    {
+//        if (i % 2 == 0) continue;
+//        sumOdd += i;
+
+//        if (sumOdd >= 1000) 
+//        { 
+//            Console.WriteLine("This number is too big!"); break; 
+//        }
+//    }
+//    Console.WriteLine($"Sum of odd numbers between 1 and {inputNumber}: {sumOdd}");
+//}
+//else
+//{
+//    Console.WriteLine("You did not enter a number");
+//}
+#endregion
